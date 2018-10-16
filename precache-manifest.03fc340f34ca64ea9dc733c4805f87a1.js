0adc5d2203e7d0ddc90cf8e9b5e6decea15af95f
Amanda R Fernandes <amandadcunha@gmail.com>
2018-10-16 07:48:54 -0400

    Updates

---
+++ precache-manifest.03fc340f34ca64ea9dc733c4805f87a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c146f403f8d53c3db34",
+    "url": "/react-shopping-checkout/static/js/runtime~main.3c146f40.js"
+  },
+  {
+    "revision": "e42967f4596f73b7f283",
+    "url": "/react-shopping-checkout/static/js/main.e42967f4.chunk.js"
+  },
+  {
+    "revision": "4b3d0d1ff8a0b0c5cebb",
+    "url": "/react-shopping-checkout/static/js/1.4b3d0d1f.chunk.js"
+  },
+  {
+    "revision": "e42967f4596f73b7f283",
+    "url": "/react-shopping-checkout/static/css/main.fcec937e.chunk.css"
+  },
+  {
+    "revision": "a0252079e491d16ceafaa6a7365bcd7b",
+    "url": "/react-shopping-checkout/index.html"
+  }
+];
